8b9dcd856f9d0154a0317ab64b8ab9c8e02d8591
hudiya.resa@gmail.com <hudiya.resa@gmail.com>
2025-07-22 13:24:37 +0700

    rev typography

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -13,15 +13,23 @@ export default function NotFound() {
           variant="h1"
           color="blue-gray"
           className="mt-10 !text-3xl !leading-snug md:!text-4xl"
+          placeholder=""
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
         >
           Error 404 <br /> It looks like something went wrong.
         </Typography>
-        <Typography className="mt-8 mb-14 text-[18px] font-normal text-gray-500 mx-auto md:max-w-sm">
+        <Typography
+          className="mt-8 mb-14 text-[18px] font-normal text-gray-500 mx-auto md:max-w-sm"
+          placeholder=""
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
+        >
           Don&apos;t worry, our team is already on it. Please try refreshing
           the page or come back later.
         </Typography>
         <Link href="/">
-          <Button color="gray" className="w-full px-4 md:w-[8rem]">
+          <Button color="gray" className="w-full px-4 md:w-[8rem]" placeholder="" onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}>
             back home
           </Button>
         </Link>
