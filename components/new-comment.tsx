b41f4d662b7dfcf848759b7dab9def8d44b265e6
hudiya.resa@gmail.com <hudiya.resa@gmail.com>
2025-07-22 15:47:54 +0700

    rev typography

--- components/new-comment.tsx
+++ components/new-comment.tsx
@@ -17,6 +17,9 @@ export function NewComment() {
         <Typography
           variant="small"
           className=" font-bold flex items-center gap-2 !text-gray-900"
+          placeholder=""      
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}          
         >
           Tina Andrew
         </Typography>
@@ -29,7 +32,11 @@ export function NewComment() {
             variant="static"
             placeholder="Write a nice reply or go home..."
           />
-          <Button color="gray" className="mt-4" size="md">
+          <Button color="gray" className="mt-4" size="md"
+            placeholder=""
+            onPointerEnterCapture={() => {}}
+            onPointerLeaveCapture={() => {}}
+          >
             button
           </Button>
         </form>
