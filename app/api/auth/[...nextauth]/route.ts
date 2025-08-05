75616de6185430b2a2387f08aa4ed66e8d5e6eb9
hudiya.resa@gmail.com <hudiya.resa@gmail.com>
2025-08-05 16:49:51 +0700

    rev nextauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,7 +7,7 @@ import { z } from "zod";
 
 const prisma = new PrismaClient();
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
