231edead6b72f238963322c74463c6edb82c4723
hudiya.resa@gmail.com <hudiya.resa@gmail.com>
2025-08-05 18:32:23 +0700

    rev nextauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -114,4 +114,6 @@ const authOptions: NextAuthOptions = {
 };
 
 const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+
+export const GET = handler;
+export const POST = handler;
